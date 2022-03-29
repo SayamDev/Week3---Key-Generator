7b15e5bcc73f4ef115532f61c690401e7c47d2d8
Sayam Ajmal <itssanesayam@gmail.com>
2022-03-29 16:15:55 +0100

    added 3rd event

--- app.js
+++ app.js
@@ -4,4 +4,23 @@ window.addEventListener("keydown", (event) => {
     let keyClick = event.key; // .key = generates an event of the key that was executed
 
     numKey.innerHTML = `${keyClick}`; //inner.html will target the text inside the numKey (which is displayKey ID)
+
+    //location part-----------------
+
+    let locKey = document.getElementById("displayLocation")
+
+    let locClick = event.location;
+
+    locKey.innerHTML = `${locClick};`
+    //location part end---------------
+
+
+
+    let codeKey = document.getElementById("displayCode")
+
+    let codeClick = event.code;
+
+    codeKey.innerHTML = `${codeClick}`
+
+
 });
